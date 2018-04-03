3f6522f65ffe423dbeb4156f517adf33452f67d7
spencer brosseau <spencer.brosseau@gmail.com>
2018-04-02 22:53:53 -0400

    update 4

---
+++ FSdev/FSdev.js
@@ -0,0 +1,7 @@
+function closeTopMenu(){
+    document.getElementById('header').style.header = 'hidden';
+  }
+
+  function openTopMenu(){
+    document.getElementById('header').style.header = '70px';
+  }
